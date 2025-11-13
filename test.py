30704e35bd016bd8560b203fa998c4a87ca33360
Oliver <oliver.giesel@gmx.de>
2025-11-13 18:16:46 +0100

    add test for log

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
 from base_car import*
 import time
 
-def fahrmodus_test(car):
-    car.speed = 30
-    car.steering_angle = 135
-    car.drive()
-    time.sleep(3)
-    car.stop()
+class Test(BaseCar):
+    def __init__(self):
+        super().__init__()
+        self.dist = 5
+
+    def get_log(self):
+        base_log = super().get_log()
+        base_log["dist"] = self.dist
+        return base_log
+    
+    def update(self):
+        self.write_log()
+
+# def fahrmodus_test(car):
+#     car.speed = 30
+#     car.steering_angle = 135
+#     car.drive()
+#     time.sleep(5)
+#     car.stop()
 
 def main():
     car = BaseCar()
-    fahrmodus_test(car)  
+    # fahrmodus_test(car) 
+    # car.fahrmodus1(selection='1')
+    a=Test()
+    a.write_log()
+    # print(a)
 
 if __name__ == "__main__":
     main()
