48c95878587b0b6ef72097e4d798d64028f92284
Oliver <oliver.giesel@gmx.de>
2025-11-14 16:22:05 +0100

    add DataLogger for testing

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from base_car import*
 import time
 
-class Test(BaseCar):
-    def __init__(self):
-        super().__init__()
+class Test(DataLogger):
+    def __init__(self,car):
+        super().__init__(car)
         self.dist = 5
 
     def get_log(self):
@@ -11,22 +11,22 @@ class Test(BaseCar):
         base_log["dist"] = self.dist
         return base_log
     
-    def update(self):
-        self.write_log()
 
-# def fahrmodus_test(car):
-#     car.speed = 30
-#     car.steering_angle = 135
-#     car.drive()
-#     time.sleep(5)
-#     car.stop()
+def fahrmodus_test(car):
+    car.speed = 30
+    car.steering_angle = 135
+    car.drive()
+    time.sleep(5)
+    car.stop()
 
 def main():
     car = BaseCar()
     # fahrmodus_test(car) 
     # car.fahrmodus1(selection='1')
-    a=Test()
+    a=Test(car)
     a.write_log()
+    # car.fahrmodus_1()
+    
     # print(a)
 
 if __name__ == "__main__":
