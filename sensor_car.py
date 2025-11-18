f22f3a21a678c9e64c3c706ef4756efac75f4c47
MaGa <MaGa.Ma@Ga>
2025-11-18 13:52:35 +0100

    Update SensorCar V3

--- sensor_car.py
+++ sensor_car.py
@@ -9,11 +9,26 @@ class SensorCar(SonicCar): # Beschreibt die Klasse "SensorCar"
     def __init__(self):
         super().__init__()
         self.infrared = Infrared(references=[134, 128, 143, 137, 121])
-        self.on_line = 0
-        self.__calibrated_reference = list # = [134, 128, 143, 137, 121]
+        self.line_pos = [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]  # Start mit leerer Liste
+                    ]
+        self.line_pos_max_len = 3    # Anzahl der gespeicherten Werte
 
-    def line_pos(self):
-        return self.infrared.read_digital()
+    def get_line_pos(self):
+        new_value = self.infrared.read_digital()  # Neuen Wert lesen
+        # Neuen Wert vorne einfügen
+        self.line_pos.insert(0, new_value)
+        # Falls Liste zu lang wird, letzten Wert entfernen
+        if len(self.line_pos) > self.line_pos_max_len:
+            self.line_pos.pop()
+        print(self.line_pos)
+        return self.line_pos[0]
+
+
+    # def get_line_pos(self):
+    #     return self.infrared.read_digital()
 
     def follow_line(self):
         '''erkennt die Position der Linie und gibt einen entsprechenden Lenkwinkel, sowie reduktion der Geschwindigkeit zurück'''
@@ -31,29 +46,19 @@ class SensorCar(SonicCar): # Beschreibt die Klasse "SensorCar"
                     [0, 0, 0, 1, 1, 135, 0.85],
                     [0, 0, 0, 0, 1, 135, 0.75]
                 ]
-        # self.__line_posible_positions = [
-        #             [0,0,0,0,0,None,None],
-        #             [1,0,0,0,0,45,0.75],
-        # old         [1,1,0,0,0,56.25,0.85],
-        #             [0,1,0,0,0,67.5,0.95],
-        #             [0,1,1,0,0,78.75,1],
-        #             [0,0,1,0,0,90,1],
-        #             [0,0,1,1,0,101.25,1],
-        #             [0,0,0,1,0,112.5,0.95],
-        #             [0,0,0,1,1,123.75,0.85],
-        #             [0,0,0,0,1,135,0.75]
-        #         ]
-
+        
         columns = ["ir_0","ir_1","ir_2","ir_3","ir_4","steering_angle","speed_reduction_to_follow"]
         #self.__line_posible_positions = pd.DataFrame(self.__line_posible_positions, columns=columns)
         #self.__line_posible_positions_temp = self.__line_posible_positions.drop(columns=["steering_angle"])
 
         self.__line_posible_positions_temp = [row[:-2] for row in self.__line_posible_positions]
-
-        irs = self.infrared.read_digital()
+        print(self.line_pos)
+        abc = self.get_line_pos()
+        print(self.line_pos)
+#        irs = self.infrared.read_digital()
         for i, row in enumerate(self.__line_posible_positions_temp):
-            print(i,row,self.line_pos())
-            if row == self.line_pos():
+            print(i,row,self.line_pos[0],self.line_pos[0])
+            if row == self.line_pos[0]:
                 print(f"pos found in line{i}")
                 break
         #steering_angle_to_follow = self.__line_posible_positions["steering_angle"][i]
@@ -80,10 +85,9 @@ class SensorCar(SonicCar): # Beschreibt die Klasse "SensorCar"
         self.__line_pos_left = [1,0,0,0,0]
         self.__line_pos_right = [0,0,0,0,1]
                 
-        if self.line_pos() == self.__line_pos_left or self.__line_pos_right:
-            self._line_pos_old = self.line_pos()
-        if self.line_pos() == self.__line_pos_right:
-            self.line_pos_old = self.line_pos()
+        if self.line_pos[0] == self.__line_pos_left or self.__line_pos_right:
+            self._line_pos_old = self.line_pos[0]
+
 
     def on_line(self):
         '''entscheidet anhand der Werte des Infrarotsensor ob das Fahrzeug auf er Line steht'''            
