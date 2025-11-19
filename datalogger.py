8b2ce26c1d2ed527fab41e3e9a44101fb1543229
Oliver <oliver.giesel@gmx.de>
2025-11-19 11:26:31 +0100

    add datalogger

---
+++ datalogger.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import time
+
+class DataLogger():
+
+    def __init__(self,car):
+        self.car = car
+
+    def get_log(self):
+        '''gibt Basislog als Dictionary zurück'''
+        self.log = {
+            "time": time.time(),
+            "speed": self.car.speed,
+            "direction": self.car.direction,
+            "steering_angle": self.car.steering_angle
+        }
+        return self.log
+
+    def write_log(self):
+        '''schreibt die logging Daten'''
+        log_entry = self.get_log()
+        self.car.logs = pd.concat([self.car.logs,pd.DataFrame([log_entry])], ignore_index=True)
+        # print(self.car.logs.head())
