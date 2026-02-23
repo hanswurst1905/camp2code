26b8ef52d35ca5716a56abe9140561b1cdf44357
Oliver <oliver.giesel@gmx.de>
2026-02-23 13:50:27 +0100

    add cam_car.py

---
+++ cam_car.py
@@ -0,0 +1,15 @@
+from software.basisklassen_cam import*
+from sensor_car import*
+import cv2
+import numpy as np
+
+class CamCar(SensorCar):
+    def __init__(self):
+        super().__init__()
+        self.camera = Camera()
+
+    def get_image(self):
+        img = self.camera.get_frame()
+        img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        return img_hsv
+        # return self.camera.get_frame()
