fff49a3f1a33835d626c78c0a1a959369debfa64
Oliver <oliver.giesel@gmx.de>
2026-03-06 21:02:04 +0100

    add exponentielle glättung Lenkwinkel

--- cnn_car.py
+++ cnn_car.py
@@ -10,7 +10,7 @@ class CnnCar(CamCar):
 
 
     def predict_steering_angle(self):
-        interpreter = tflite.Interpreter(model_path="PiCar_model.tflite") # path=Pfad zur .tflite Datei
+        interpreter = tflite.Interpreter(model_path="PiCar_model_260305_1853.tflite") # path=Pfad zur .tflite Datei
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
         interpreter.allocate_tensors()
@@ -18,6 +18,13 @@ class CnnCar(CamCar):
         # img = cv2.imread("/home/pi/scripts/PiCar/pictures/2026-02-27_18_41_44.804_10000000210c65be_121.jpg")
         self.get_image()
         img = self.image
+
+        # fac_t, fac_b = 0.28,0
+        # h, w = img.shape[:2]
+        # bottom = int(h - fac_b*h)
+        # top = int(fac_t*h)
+        # img_cropped = img[top:bottom,:,:]
+
         new_img = cv2.resize(img, (224, 224))
         new_img = new_img.astype(np.float32)
         new_img /= 255.
@@ -32,7 +39,8 @@ class CnnCar(CamCar):
     def fahrmodus_cnn(self):
         while True:
             self.get_image()
-            self.steering_angle = self.predict_steering_angle()
+            angle_predict = self.predict_steering_angle()
+            self.steering_angle = self.glaettung_alpha(steering_angle=angle_predict, alpha=0.8)
             self.save_image()
             if self.state == "stop":
                 print("CamCar Ende")
